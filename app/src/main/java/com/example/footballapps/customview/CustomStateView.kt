3a8ec940852d21c7bbe5a4b0ffcf99b8ca6d2aed
Dwi Bonggo <dwibonggo25@gmail.com>
2019-11-18 09:33:48 +0700

    Add custom view'

--- app/src/main/java/com/example/footballapps/customview/CustomStateView.kt
+++ app/src/main/java/com/example/footballapps/customview/CustomStateView.kt
@@ -3,8 +3,64 @@ package com.example.footballapps.customview
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.footballapps.R
 
-class CustomStateView (context: Context, attrs: AttributeSet): View (context, attrs){
+
+class CustomStateView : ConstraintLayout {
+
+    lateinit var tvMessage: TextView
+    lateinit var progres: ProgressBar
+    lateinit var tvTryAgain: TextView
+    lateinit var view: View
+
+    lateinit var message: String
+
+    constructor(context: Context) : super(context) {
+        init(null)
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        init(attributeSet)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init(attrs)
+    }
+
+    private fun init(set: AttributeSet?) {
 
 
+        if (set == null) {
+            return
+        }
+
+        val ta = context.obtainStyledAttributes(set, R.styleable.CustomStateView)
+
+        ta.recycle()
+
+        declareView()
+    }
+
+    private fun declareView() {
+        view = View.inflate(context, R.layout.custom_view, this)
+        tvMessage = view.findViewById(R.id.tvMessage)
+        tvTryAgain = view.findViewById(R.id.tvTryAgain)
+        progres = view.findViewById(R.id.progressBar)
+        showLoading()
+    }
+
+    private fun hideLoading () {
+        progres.visibility = View.GONE
+    }
+
+    private fun showLoading () {
+        progres.visibility = View.VISIBLE
+    }
 }
